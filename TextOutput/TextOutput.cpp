9f1837ee17602f647af806278aafa883e9f56d04
Álvaro Real <34766606+Darkenend@users.noreply.github.com>
2021-03-07 02:37:27 +0100

    Getting Started

---
+++ TextOutput/TextOutput.cpp
@@ -0,0 +1,15 @@
+// TextOutput.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Let's output some text
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Starting program... " << flush;
+    cout << "Welcome to Paradise, Arizona!" << endl;
+    cout << "We have a population of 42,069. " << flush;
+    cout << "Enjoy your stay!" << endl;
+    return 0;
+}
